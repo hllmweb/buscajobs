4394f046715f184e813414bbc80a7363a8ce94d3
Hugo Mesquita <hugomesquitaweb@gmail.com>
2021-05-30 17:05:22 -0400

    lista cidades

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,35 @@
 import React from 'react';
+import axios from 'axios';
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
 
+const api = axios.create({
+    baseURL: `https://api-buscajobs.herokuapp.com/cidade/lstCidade/`
+});
+
+
+
 export default class App extends React.Component{
+
+    state = {
+        cidades: []
+    }
+
+    constructor(){
+        super();
+        api.get('/').then(res=> {
+            console.log(res.data)
+            this.setState({ cidades: res.data})
+        })
+    }
+
     render(){
         return(
             <div className="header">
                 <Header />
                 <Main />
+                {this.state.cidades.map(cidade => <h2 key={cidade.id}>{cidade.cidade}</h2>)}
                 <br></br><br></br><br></br><br></br>
                 <br></br><br></br><br></br><br></br>
                 <br></br><br></br><br></br><br></br>
